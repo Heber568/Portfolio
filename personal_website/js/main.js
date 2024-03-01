a599848bba6eda27968ceb23828a2ee086f88ce0
Herbert <99870586+Heber568@users.noreply.github.com>
2024-03-01 21:56:37 +0300

    Add files via upload

--- personal_website/js/main.js
+++ personal_website/js/main.js
@@ -1,1 +1,23 @@
-
+/* Только после загрузки страницы выполнить код. */ 
+document.addEventListener('DOMContentLoaded', function () {
+    /* Показать/скрыть мобильное менью */ 
+    const headerMenu = document.querySelector('#header-menu');
+    const menuToggle = document.querySelector('#mobile-icon');
+    const bodyEl = document.body;
+    menuToggle.addEventListener('click', function() {
+        if(this.classList.contains('active')) {
+            this.classList.remove('active')
+            headerMenu.classList.remove('open')
+            bodyEl.classList.remove('lock')
+        } else {
+            this.classList.add('active');
+            headerMenu.classList.add('open')
+            bodyEl.classList.add('lock')
+        }
+    });
+    headerMenu.addEventListener('click', function () {
+        this.classList.remove('open');
+        menuToggle.classList.remove('active');
+        bodyEl.classList.remove('lock');
+    });
+})
